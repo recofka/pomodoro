86bf563be156b4a07cee741d65498f4ce041aea4
Denise Recofka <recofka@gmail.com>
2023-07-04 18:49:54 +0200

    Adding and styling History page

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,80 @@
+import { HistoryContainer, HistoryList, Status } from './styles';
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>My History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Task</th>
+              <th>Duration</th>
+              <th>Duration</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="red">Interrupted</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="yellow">In Progress</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Task</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutes</td>
+              <td>2 months ago</td>
+              <td>
+                <Status statusColor="green">Completed</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  );
+}
